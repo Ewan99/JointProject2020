83f37eb7c90cb7b06703d5b0ca1957e6605b3f59
Ewan Duff <ewan4d@gmail.com>
2021-03-01 04:22:21 +0000

    -Redesign Homepage -Form now Encrypts input and saves to DB -Login Page can retrieve and decrypt records

--- form.php
+++ form.php
@@ -7,88 +7,157 @@ $conn = new mysqli($host, $username, $password);
 $cipher = 'AES-128-CBC';
 $key = 'thebestsecretkey';
 
-if ($conn->connect_error) {
+if ($conn->connect_error)
+{
   die('Connection failed: ' . $conn->connect_error);
 }
 
-if (isset($_POST['delete-everything'])) {
-  $sql = 'DROP DATABASE securenotepad;';
-  if (!$conn->query($sql) === TRUE) {
-    die('Error dropping database: ' . $conn->error);
-  }
-}
-
-$sql = 'CREATE DATABASE IF NOT EXISTS securenotepad;';
-if (!$conn->query($sql) === TRUE) {
+$sql = 'CREATE DATABASE IF NOT EXISTS clientsdb;';
+if (!$conn->query($sql) === TRUE)
+{
   die('Error creating database: ' . $conn->error);
 }
 
-$sql = 'USE securenotepad;';
-if (!$conn->query($sql) === TRUE) {
+$sql = 'USE clientsdb;';
+if (!$conn->query($sql) === TRUE)
+{
   die('Error using database: ' . $conn->error);
 }
 
-$sql = 'CREATE TABLE IF NOT EXISTS notes (
-id int NOT NULL AUTO_INCREMENT,
-iv varchar(32) NOT NULL,
-content varchar(256) NOT NULL,
-PRIMARY KEY (id));';
-if (!$conn->query($sql) === TRUE) {
-  die('Error creating table: ' . $conn->error);
+$sql = ' CREATE TABLE IF NOT EXISTS `clients`
+  (
+  `Client No.` int(8) NOT NULL auto_increment,
+  `First Name` varchar(250)  NOT NULL default "",
+  `Last Name` varchar(250)  NOT NULL default "",
+  `Address` varchar(250)  NOT NULL default "",
+  `PPSN` varchar(250),
+  `Phone`  varchar(250),
+  `Email` varchar(250)  NOT NULL default "",
+  `DOB`varchar(250),
+  `iv` varchar(250),
+   PRIMARY KEY  (`Client No.`)
+  ); ';
+if (!$conn->query($sql) === TRUE)
+{
+  die('Error creating database: ' . $conn->error);
 }
+
+
 ?>
 <html>
-<head>
-<title>Secure Notepad</title> </head>
-<body>
-<h1>Secure Notepad</h1>
-<?php
-if (isset($_POST['new-note'])) {
-  $iv = random_bytes(16);
-  $escaped_content = $conn -> real_escape_string($_POST['content']);
-  $encrypted_content = openssl_encrypt($escaped_content, $cipher, $key, OPENSSL_RAW_DATA, $iv);
-  $iv_hex = bin2hex($iv);
-  $content_hex = bin2hex($encrypted_content);
-  $sql = "INSERT INTO notes (iv, content) VALUES ('$iv_hex', '$content_hex')";
-  if ($conn->query($sql) === TRUE) {
-    echo '<p><i>New note added!</i></p>';
-  } else {
-    die('Error creating note: ' . $conn->error);
-  }
-}
-?>
-<h2>Create a New Note</h2>
+  <head>
+    <link rel="stylesheet" type="text/css" href="form.css">
+    </style>
 
-<form method="post">
-  <input type="text" id="content" name="content" size="64"><br><br>
-  <button type="submit" name="new-note">Create Note</button>
-</form>
+    <script>
+    function checkEmail(input)
+    {
+    	if(input.value != document.getElementById('Email').value) // Comparison statement
+    		{
+    			input.setCustomValidity('The two email addresses must match!');
+    		}
+    	else
+    		{
+    			//Input is valid -- reset error message
+    			input.setCustomValidity('');
+    		}
+    }
+    </script>
 
-<h2>List Existing Notes</h2>
 
-<?php
-$sql = "SELECT id, iv, content FROM notes";
-$result = $conn->query($sql);
+  </head>
 
-if ($result->num_rows > 0) {
-  echo '<table><tr><th>ID</th><th>Content</th></tr>';
-  while($row = $result->fetch_assoc()) {
-    $id = $row['id'];
-    $iv = hex2bin($row['iv']);
-    $content = hex2bin($row['content']);
-    $unencrypted_content = openssl_decrypt($content, $cipher, $key, OPENSSL_RAW_DATA, $iv);
-    echo "<tr><td>$id</td><td>$unencrypted_content</td></tr>";
-  }
-  echo '</table>';
-} else {
-  echo '<p>There are no notes!</p>';
-}
-?>
+  <body>
 
-<h3>Delete Everything</h3>
+    <?php
+    if (isset($_POST['new-record'])) {
+      $iv = random_bytes(16);
 
-<form method="post">
-  <button type="submit" name="delete-everything">Delete Everything!</button>
-</form>
-</body>
+      $enc_fname = $conn -> real_escape_string($_POST['firstname']);
+      $enc_fname = openssl_encrypt($enc_fname, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $fname_hex = bin2hex($enc_fname);
+
+      $enc_lname = $conn -> real_escape_string($_POST['lastname']);
+      $enc_lname = openssl_encrypt($enc_lname, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $lname_hex = bin2hex($enc_lname);
+
+      $enc_address = $conn -> real_escape_string($_POST['address']);
+      $enc_address = openssl_encrypt($enc_address, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $address_hex = bin2hex($enc_address);
+
+      $enc_ppsn = $conn -> real_escape_string($_POST['medNum']);
+      $enc_ppsn = openssl_encrypt($enc_ppsn, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $ppsn_hex = bin2hex($enc_ppsn);
+
+      $enc_phone = $conn -> real_escape_string($_POST['phoneNum']);
+      $enc_phone = openssl_encrypt($enc_phone, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $phone_hex = bin2hex($enc_phone);
+
+      $enc_email = $conn -> real_escape_string($_POST['email']);
+      $enc_email = openssl_encrypt($enc_email, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $email_hex = bin2hex($enc_email);
+
+      $enc_dob = $conn -> real_escape_string($_POST['dateofBirth']);
+      $enc_dob = openssl_encrypt($enc_dob, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+      $dob_hex = bin2hex($enc_dob);
+
+      $iv_hex = bin2hex($iv);
+      $sql = "INSERT INTO clients (`Client No.`, `First Name`, `Last Name`, `Address`, `PPSN`, `Phone`, `Email`, `DOB`, `iv`) VALUES (NULL, '$fname_hex', '$lname_hex', '$address_hex', '$ppsn_hex', '$phone_hex', '$email_hex', '$dob_hex', '$iv_hex')";
+      if ($conn->query($sql) === TRUE)
+      {
+        header("Location: /jp");
+        die('Profile created successfully! You will now return to the main page');
+      }
+      else
+      {
+        die('There was an error creating your record.  Please try again!   Error: ' . $conn->error);
+      }
+
+    }
+    ?>
+
+    <div class = "container">
+
+      <form>
+        <h2>Profile Registration Form</h2>
+        <h4>Welcome! Please create your medical profile before your appointment</h4><br>
+  	     <div class="inputbox"><label for="FirstName">First Name:</label>
+  	        <input title="Name must only contain alphabetic characters" type="text" name="firstname" required id="FirstName" pattern="[a-zA-Z|\s|.]+"/>
+        </div>
+
+  	     <div class="inputbox"><label for="LastName">Last Name:</label>
+  	        <input title="Name must only contain alphabetic characters" type="text" name="lastname" required id="LastName" pattern="[a-zA-Z|\s|.]+"/>
+        </div>
+
+  	     <div class="inputbox"><label for="Address">Address:</label>
+  	        <input title="Address must only contain the following: a-Z , - , 0-9 , . , , )" type="text" name="address" required id="Address" pattern="[a-zA-Z|,|\s|.|0-9]+"/>
+        </div>
+
+  	     <div class="inputbox"><label for="MedNum">Medical Card Number:</label>
+  	        <input type="text" title="Must contain 7 digits followed by one or two characters" name="medNum" required id="MedNum" pattern="(\d{7})([A-Z]{1,2})"/>
+        </div>
+
+  	    <div class="inputbox"><label for="PhoneNum">Phone Number:</label>
+    	     <input title="Valid Format : 0871234567" type="tel" name="phoneNum" required id="PhoneNum" pattern="[0-9]{10}"/>
+        </div>
+
+  	    <div class="inputbox"><label for="Email">Email Address:</label>
+  	       <input type="email" name="email" required id="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
+        </div>
+
+  	    <div class="inputbox"><label for="VerifyEmail">Confirm Email:</label>
+  	       <input type="email" name="verifyEmail" required id="VerifyEmail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" oninput=checkEmail(VerifyEmail) />
+        </div>
+
+  	    <div class="inputbox"><label for="DateOfBirth">Date of Birth:</label>
+  	       <input type="date" name="dateofBirth" required id="DateOfBirth" oninput="checkAge(DateOfBirth)" />
+        </div>
+  	    <br>
+  	    <div class="myButton">
+  	       <input type="submit" formmethod="post" value="Submit Form" name="new-record" />
+  	    </div>
+      </form>
+    </div>
+
+  </body>
 </html>
---
+++ index.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+</style>
+</head>
+
+<body>
+<<?php
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$conn = new mysqli($host, $username, $password);
+
+$cipher = 'AES-128-CBC';
+$key = 'thebestsecretkey';
+
+if ($conn->connect_error)
+{
+  die('Connection failed: ' . $conn->connect_error);
+}
+
+$sql = 'CREATE DATABASE IF NOT EXISTS clientsdb;';
+if (!$conn->query($sql) === TRUE)
+{
+  die('Error creating database: ' . $conn->error);
+}
+
+$sql = 'USE clientsdb;';
+if (!$conn->query($sql) === TRUE)
+{
+  die('Error using database: ' . $conn->error);
+}
+
+$sql = ' CREATE TABLE IF NOT EXISTS `clients`
+  (
+  `Client No.` int(8) NOT NULL auto_increment,
+  `First Name` varchar(250)  NOT NULL default "",
+  `Last Name` varchar(250)  NOT NULL default "",
+  `Address` varchar(250)  NOT NULL default "",
+  `PPSN` varchar(250),
+  `Phone`  varchar(250),
+  `Email` varchar(250)  NOT NULL default "",
+  `DOB`varchar(250),
+  `iv` varchar(250),
+   PRIMARY KEY  (`Client No.`)
+  ); ';
+if (!$conn->query($sql) === TRUE)
+{
+  die('Error creating database: ' . $conn->error);
+}
+
+
+if (isset($_POST['validate']))
+{
+	$sql = "SELECT `Client No.`, `PPSN`, `iv` FROM clients";
+	$result = $conn->query($sql);
+	$found = "false";
+	if ($result->num_rows > 0)
+	{
+	  echo '<table><tr><th>ID</th><th>Content</th></tr>';
+	  while($row = $result->fetch_assoc())
+		{
+			if($found = false)
+			{
+				$clientno = $row['Client No.'];
+		    $ppsn_hex = hex2bin($row['PPSN']);
+		    $iv = hex2bin($row['iv']);
+		    $ppsn = openssl_decrypt($ppsn_hex, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+		    echo "<tr><td>$clientno</td><td>$ppsn</td></tr>";
+				if($ppsn == $_POST['medNum'])
+				{
+					echo "MATCH";
+					$found = "true";
+				}
+			}
+		}
+		if($found = "false")
+		{
+			 echo '<p>Error! Profile not found...  Have you registered Before? If so, please check your PPS Number again.</p>';
+		}
+	}
+
+	echo '</table>';
+}
+?>
+
+<div class = "container">
+	<form>
+		<h2>Medical Check-in</h2>
+		<h4>Welcome! Please check-in with a questionnare before your appointment</h4><br>
+
+	 	<div class="inputbox"><label for="MedNum">Medical Card Number:</label>
+			 <input type="text" title="Must contain 7 digits followed by one or two characters" name="medNum" required id="MedNum" pattern="(\d{7})([A-Z]{1,2})"/>
+	 	</div>
+
+		<input type="submit" formmethod="post" value="Log-in" name="validate" />
+
+		<br><br>
+		<h3 style="padding-left:32%;">New user? Register below...</h3>
+		<input type="button" value="Register Profile" onclick="location.href='form.php';" />
+	</form>
+</div>
+
+</body>
+</html>
